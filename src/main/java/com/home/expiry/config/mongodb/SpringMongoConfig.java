58f4b8fbb958ea298ebc9282445e112cc56ed7bc
aokicassio <newworldshadows2011>
2019-07-25 20:47:13 +0100

    Spring Profile adjustments

--- src/main/java/com/home/expiry/config/mongodb/SpringMongoConfig.java
+++ src/main/java/com/home/expiry/config/mongodb/SpringMongoConfig.java
@@ -3,6 +3,7 @@ package com.home.expiry.config.mongodb;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
@@ -10,6 +11,7 @@ import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import com.mongodb.MongoClient;
 
 @Configuration
+@Profile("local")
 public class SpringMongoConfig {
 
     @Value("${mongodb.db.expiry}")
